7d2254d5a86be7babd0683339a77b171411d6dcb
kiran-21 <kiranksr.sr@gmail.com>
2024-12-31 10:48:44 -0500

    Update basic layout v2

--- src/App.js
+++ src/App.js
@@ -1,16 +1,9 @@
 import './App.css';
 import Home from './components/Home';
 import Header from './components/Header';
-import Container from "@mui/material/Container";
-// import Main from './components/Project';
-// import About from './components/About';
-// import Navigation from './components/Navigation';
+
 
 import Footer from './components/Footer';
-import { Height } from '@mui/icons-material';
-// import { Route, Routes, Router } from 'react-router';
-
-// import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 
 
 function App() {
--- src/components/Header.js
+++ src/components/Header.js
@@ -1,8 +1,6 @@
 import AppBar from '@mui/material/AppBar';
 import Toolbar from '@mui/material/Toolbar';
 import Typography from '@mui/material/Typography';
-import IconButton from '@mui/material/IconButton';
-import MenuIcon from '@mui/icons-material/Menu';
 
 const Header = ()=>{
 
