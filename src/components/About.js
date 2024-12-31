1bbffaff2b6d6d88edb436feba2ad01fa16c9ea1
kiran-21 <kiranksr.sr@gmail.com>
2024-12-31 10:59:23 -0500

    small fix

--- src/components/About.js
+++ src/components/About.js
@@ -2,7 +2,9 @@ export default function About () {
 
     return(
         <>
-        this is about page this is header</>
+        this is about page this is header,
+        
+        this is about sections pages......</>
     
     )
 }
